f2536ca22dad17a170fab1bd71b129dbb7aaf7c5
leonardogedler <leogedler@hotmail.com>
2017-01-18 18:42:39 -0400

    Added routes

---
+++ src/components/post_new.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class PostNew extends Component {
+    render(){
+        return (
+            <div>Create post</div>
+        );
+    }
+}
+
+export default PostNew;
