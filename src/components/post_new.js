d7f77fb003229dc5115369f8d2b5f9f35544d0e9
leonardogedler <leogedler@hotmail.com>
2017-01-18 23:12:16 -0400

    CRUD operations

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,12 +1,79 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form'; 
+import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostNew extends Component {
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    onSubmit(props){
+        this.props.createPost(props)
+            .then(()=>{
+                // Blog post have been created, navigate the user to the index
+                this.context.router.push('/');
+            })
+    };
+    
     render(){
+
+        const { fields: { title, categories, content }, handleSubmit } = this.props;
+
         return (
-            <div>Create post</div>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <h3>Create a new post</h3>
+
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
+                    <label>Title</label>
+                    <input type="text" className="form-control" {...title} />
+                    <div className="text-helper">
+                        {title.touched ? title.error : ''}
+                    </div>
+                </div>
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
+                    <label>Categories</label>
+                    <input type="text" className="form-control" {...categories} />
+                    <div className="text-helper">
+                        {categories.touched ? categories.error : ''}
+                    </div>
+                </div>
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
+                    <label>Content</label>
+                    <textarea type="text" className="form-control" {...content} />
+                    <div className="text-helper">
+                        {content.touched ? content.error : ''}
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
+            </form>
         );
     }
 }
 
-export default PostNew;
+function validate(values){
+    const errors = {};
+
+    if(!values.title){
+        errors.title = 'Enter a title';
+    }
+    if(!values.categories){
+        errors.categories = 'Enter a categories';
+    }
+    if(!values.content){
+        errors.content = 'Enter some content';
+    }
+
+    return errors;
+}
+
+// connect: 1st argument is mapStateToProps, 2nd argument is mapDispatchToProps
+// reduxForm: 1st argument form config, 2nd argument is mapStateToProps, 3rd is mapDispatchToProps
+
+export default reduxForm ({
+    form: 'PostNewForm',
+    fields:['title', 'categories', 'content'],
+    validate
+}, null, { createPost })(PostNew);
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -7,6 +7,21 @@ import { Link } from 'react-router';
 class PostsIndex extends Component{
     componentWillMount(){
         this.props.fetchPosts();
+    };
+
+
+
+    renderPosts(){
+        return this.props.posts.map((post)=>{
+            return (
+                <li className="list-group-item" key={post.id}>
+                    <Link to={'post/'+post.id}>
+                        <span className="pull-xs-right">{post.categories}</span>
+                        <strong>{post.title}</strong>
+                    </Link>        
+                </li>
+            );
+        })
     }
 
 
@@ -18,14 +33,21 @@ class PostsIndex extends Component{
                         Add a Post
                     </Link>
                 </div>
-                List of blog posts
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         )
     };
+}
+
+function mapStateToProps(state){
+    return { posts: state.posts.all  }
 }
 
 // function mapDispatchToProps(dispatch){
 //     return bindActionCreators({fetchPosts}, dispatch);
 // }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
